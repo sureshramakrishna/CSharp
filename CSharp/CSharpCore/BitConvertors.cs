3d509a27f8840e181090c19fba11a04f391c05da
Suresh Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-10-18 18:53:03 +0530

    BitConvertor RangeAndIndexers MemoryMappedFiles

---
+++ CSharp/CSharpCore/BitConvertors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitConvertors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = BitConverter.GetBytes(1);
+            int resultInt = BitConverter.ToInt32(bytes);
+            bool resultBool = BitConverter.ToBoolean(bytes);
+            char resultChar = BitConverter.ToChar(bytes);
+            double resultDouble = BitConverter.ToDouble(bytes);
+            short resultInt16 = BitConverter.ToInt16(bytes);
+            long resultInt64 = BitConverter.ToInt64(bytes);
+            ushort resultUInt16 = BitConverter.ToUInt16(bytes);
+            uint resultUInt32 = BitConverter.ToUInt32(bytes);
+            ulong resultUInt64 = BitConverter.ToUInt64(bytes);
+        }
+    }
+}
---
+++ CSharp/CSharpCore/RangeAndIndexers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Enumerable.Range(1, 10).ToArray();
+            var lastElement = array[^1]; //Same as array[array.Length - 1]
+            int[] range = array[0..2]; //Set range to 1, 2
+
+            int i = 0, j = 2;
+            range = array[i..j];
+            range = array[0..^0]; //range from 1 to 10. Note that ^0 does not throw error bcz last range is exclusive.  
+            range = array[0..^j]; //range from 1 to 8 
+        }
+    }
+}
